8312a2fc5f6f25754919ef00409d70c3cd692e5a
robmargo <margolies.rob@gmail.com>
2019-12-20 10:27:50 -0500

    Add files via upload

---
+++ sort_transactions.py
@@ -0,0 +1,66 @@
+import sys
+import re
+
+# Usage:
+# python sort_transactions.py finances.beancount >  finances.beancount.tmp
+# bean-check finances.beancount.tmp
+# mv finances.beancount.tmp finances.beancount
+
+def get_date(tx):
+  return tx[0][0:10]
+
+f = open(sys.argv[1], "r")
+postings = []
+tx = ""
+transactions = []
+unknown_lines = []
+skipped_lines = []
+skip_lines = True
+for line in f.readlines():
+
+  if re.match(r".*TRANSACTIONS", line):
+    skipped_lines.append(line)
+    skip_lines=False
+    continue
+
+  if skip_lines == True:
+    skipped_lines.append(line)
+    continue
+
+  if re.match(r"\d\d\d\d-\d\d-\d\d", line):
+    if len(tx) > 0 :
+      transactions.append((tx, postings))
+    tx = line
+    postings = []
+  elif re.match(r"\s+\S.*\n", line):
+    postings.append(line)
+  elif re.match("\n", line):
+    if(len(line) > 1) :
+      # This is something weird and we need to handle it
+      print(line, file=sys.stderr)
+  else:
+    unknown_lines.append(line)
+
+# Dont forget to add the last transaction
+if len(tx) > 0 :
+  transactions.append((tx, postings))
+
+# Print the header stuff
+for line in skipped_lines:
+  print(line, end = '')
+
+# Sort and print all the remaining directives, separated by a newline
+transactions.sort(key=get_date)
+for [tx, postings] in transactions:
+  print(tx, end ='')
+  for posting in postings:
+    print(posting, end = '')
+  print()
+
+
+# Print to stderr any messages that will be deleted
+if len(unknown_lines)>0:
+  print("Found %d Lines that dont match anything that will be deleted." % len(unknown_lines), file=sys.stderr)
+  for line in unknown_lines:
+    print(line, file = sys.stderr)
+
